46a030a7d683aa5a26aeede9072028aeaebddfba
MariyahW <149527258+MariyahW@users.noreply.github.com>
2024-01-25 10:45:29 -0500

    formatting of site

--- site.js
+++ site.js
@@ -1,5 +1,13 @@
-function otherRequired(other,textBox ){
-    if (other== true) {
-        textBox
-    }
-}
+const submitted=document.getElementById('submitted');
+const formButton=document.getElementById('conForm');
+// let name= document.getElementById('name');
+let email= document.getElementById('email');
+let phone= document.getElementById('phone');
+let cR= document.getElementById('contactReason');
+let formText= document.getElementById('textBox');
+
+formButton.addEventListener('submit',event=>{
+    event.preventDefault();
+submitted.style.display="block";
+console.log(event);
+})
